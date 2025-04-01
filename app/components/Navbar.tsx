b55a0b1e3ab1459c16c15213406d08168f2a50a5
Witek <wiktorskid@wp.pl>
2025-04-01 17:41:28 +0200

    Fix: mobile nav clickability

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -123,7 +123,7 @@ interface MobileNavProps {
 
 function MobileNav({gameMode, setMobileNav, mobileNav, isAnimating, handleGameModeChange}: MobileNavProps) {
   return (
-    <div className={`${mobileNav ? "pointer-events-auto" : "pointer-events-none"}`}>
+    <div className='pointer-events-auto lg:pointer-events-none'>
       <div className='flex lg:hidden justify-between items-center w-full px-4 py-8 '>
         <div className='flex space-x-2 ml-auto'>
           <div
@@ -145,121 +145,123 @@ function MobileNav({gameMode, setMobileNav, mobileNav, isAnimating, handleGameMo
           </div>
         </div>
       </div>
-      <div className='absolute top-64 left-0 inset-0 flex items-center justify-center sm:justify-end z-[-1] bg-red-500'>
-        <motion.div
-          className=' z-[-1] flex flex-col md:w-[450px] w-[90%] max-w-[450px] mt-32 left-[-1/2] transform translate-x-[50%] sm:mr-6   text-center '
-          initial={{opacity: 0, x: 50}}
-          animate={{
-            opacity: mobileNav ? 1 : 0,
-            x: mobileNav ? 0 : 50,
-          }}
-          exit={{opacity: 0, x: 50}}
-          transition={{
-            duration: 0.3,
-            ease: "easeInOut",
-          }}
-        >
-          <div
-            className={` w-full  text-slate-300  rounded-t-2xl  font-bold flex justify-start items-center ${
-              gameMode ? "bg-[#141414]" : "bg-slate-800"
-            }`}
+      {mobileNav && (
+        <div className='absolute top-12 left-0 w-full flex items-center justify-center sm:justify-end z-[-1] '>
+          <motion.div
+            className=' z-[-1] flex flex-col md:w-[450px] w-[90%] max-w-[450px] mt-32 left-[-1/2] transform translate-x-[50%] sm:mr-6   text-center '
+            initial={{opacity: 0, x: 50}}
+            animate={{
+              opacity: mobileNav ? 1 : 0,
+              x: mobileNav ? 0 : 50,
+            }}
+            exit={{opacity: 0, x: 50}}
+            transition={{
+              duration: 0.3,
+              ease: "easeInOut",
+            }}
           >
-            <div className='w-[60px] border-l-2 border-t-2 py-8  border-ForegroundColor rounded-tl-2xl' />
-            <h6 className='px-4 relative right-12'>Navigation</h6>
-          </div>
-          <ul className='flex flex-col'>
-            <li
-              className={`w-full  text-slate-300 py-4 border-b-[1px] ${
-                gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
-              } px-6 font-medium flex justify-between items-center`}
-            >
-              <ScrollLink
-                to='Home'
-                smooth={true}
-                duration={200}
-                offset={0}
-                spy={true}
-                onClick={() => setMobileNav(false)}
-              >
-                Home
-              </ScrollLink>
-              <IoMdHome size={25} />
-            </li>
-            <li
-              className={`w-full  text-slate-300 py-4 border-b-[1px] ${
-                gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
-              } px-6 font-medium flex justify-between items-center`}
-            >
-              <ScrollLink
-                to='Tech'
-                smooth={true}
-                duration={200}
-                offset={-100}
-                spy={true}
-                onClick={() => setMobileNav(false)}
-              >
-                {gameMode ? "Goats" : "Tech"}
-              </ScrollLink>
-              {gameMode ? <IoIosPodium size={25} /> : <FaCode size={25} />}
-            </li>
-            <li
-              className={`w-full  text-slate-300 py-4 border-b-[1px] ${
-                gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
-              } px-6 font-medium flex justify-between items-center`}
-            >
-              <ScrollLink
-                to='Works'
-                smooth={true}
-                duration={200}
-                offset={-100}
-                spy={true}
-                onClick={() => setMobileNav(false)}
-              >
-                {gameMode ? "Rescently played" : "Works"}
-              </ScrollLink>
-              <GrProjects size={25} />
-            </li>{" "}
-            <li
-              className={`w-full  text-slate-300 py-4 border-b-[1px] ${
-                gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
-              } px-6 font-medium flex justify-between items-center`}
-            >
-              <ScrollLink
-                to='Resume'
-                smooth={true}
-                duration={200}
-                offset={-100}
-                spy={true}
-                onClick={() => setMobileNav(false)}
-              >
-                {gameMode ? "Steam" : "Resume"}
-              </ScrollLink>
-              <SiReaddotcv size={25} />
-            </li>
             <div
-              className={`w-full  text-slate-300  rounded-b-2xl  font-medium flex justify-between items-center ${
-                gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
+              className={` w-full  text-slate-300  rounded-t-2xl  font-bold flex justify-start items-center ${
+                gameMode ? "bg-[#141414]" : "bg-slate-800"
               }`}
             >
-              <ScrollLink
-                to='Contact'
-                className='px-6'
-                smooth={true}
-                duration={200}
-                offset={-100}
-                spy={true}
-                onClick={() => setMobileNav(false)}
-              >
-                Contact
-              </ScrollLink>
-              <div className='flex items-center'>
-                <GrContact size={25} className='relative left-8' />
-                <div className='w-[60px] border-r-2 border-b-2 py-8  border-ForegroundColor rounded-br-2xl' />
-              </div>
+              <div className='w-[60px] border-l-2 border-t-2 py-8  border-ForegroundColor rounded-tl-2xl' />
+              <h6 className='px-4 relative right-12'>Navigation</h6>
             </div>
-          </ul>
-        </motion.div>
-      </div>
+            <ul className='flex flex-col'>
+              <li
+                className={`w-full  text-slate-300 py-4 border-b-[1px] ${
+                  gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
+                } px-6 font-medium flex justify-between items-center`}
+              >
+                <ScrollLink
+                  to='Home'
+                  smooth={true}
+                  duration={200}
+                  offset={0}
+                  spy={true}
+                  onClick={() => setMobileNav(false)}
+                >
+                  Home
+                </ScrollLink>
+                <IoMdHome size={25} />
+              </li>
+              <li
+                className={`w-full  text-slate-300 py-4 border-b-[1px] ${
+                  gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
+                } px-6 font-medium flex justify-between items-center`}
+              >
+                <ScrollLink
+                  to='Tech'
+                  smooth={true}
+                  duration={200}
+                  offset={-100}
+                  spy={true}
+                  onClick={() => setMobileNav(false)}
+                >
+                  {gameMode ? "Goats" : "Tech"}
+                </ScrollLink>
+                {gameMode ? <IoIosPodium size={25} /> : <FaCode size={25} />}
+              </li>
+              <li
+                className={`w-full  text-slate-300 py-4 border-b-[1px] ${
+                  gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
+                } px-6 font-medium flex justify-between items-center`}
+              >
+                <ScrollLink
+                  to='Works'
+                  smooth={true}
+                  duration={200}
+                  offset={-100}
+                  spy={true}
+                  onClick={() => setMobileNav(false)}
+                >
+                  {gameMode ? "Rescently played" : "Works"}
+                </ScrollLink>
+                <GrProjects size={25} />
+              </li>{" "}
+              <li
+                className={`w-full  text-slate-300 py-4 border-b-[1px] ${
+                  gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
+                } px-6 font-medium flex justify-between items-center`}
+              >
+                <ScrollLink
+                  to='Resume'
+                  smooth={true}
+                  duration={200}
+                  offset={-100}
+                  spy={true}
+                  onClick={() => setMobileNav(false)}
+                >
+                  {gameMode ? "Steam" : "Resume"}
+                </ScrollLink>
+                <SiReaddotcv size={25} />
+              </li>
+              <div
+                className={`w-full  text-slate-300  rounded-b-2xl  font-medium flex justify-between items-center ${
+                  gameMode ? "bg-[#1f1f1f]" : "bg-slate-900"
+                }`}
+              >
+                <ScrollLink
+                  to='Contact'
+                  className='px-6'
+                  smooth={true}
+                  duration={200}
+                  offset={-100}
+                  spy={true}
+                  onClick={() => setMobileNav(false)}
+                >
+                  Contact
+                </ScrollLink>
+                <div className='flex items-center'>
+                  <GrContact size={25} className='relative left-8' />
+                  <div className='w-[60px] border-r-2 border-b-2 py-8  border-ForegroundColor rounded-br-2xl' />
+                </div>
+              </div>
+            </ul>
+          </motion.div>
+        </div>
+      )}
     </div>
   );
 }
